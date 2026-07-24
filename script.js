3cf7022c26a87cd9d30f5a26cae2dd40a4b50da1
RandomlyReaper18 <140783342+RandomlyReaper18@users.noreply.github.com>
2026-07-24 09:52:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ form.addEventListener('submit', (e) => {
 });
 
 function send_message(message) {
-    const telegramBotToken = import.meta.env.VITE_TELEGRAM_BOT_APIKEY;
+    const telegramBotToken = process.env.VITE_TELEGRAM_BOT_APIKEY;
     const chatId = '6737958161';
     const url = `https://api.telegram.org/bot${telegramBotToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(message)}`;
 
